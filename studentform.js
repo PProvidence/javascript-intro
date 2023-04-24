36d27c66e1838611037a5ba5d0850712a1e15b40
PProvidence <providentialprov1@gmail.com>
2023-04-24 07:07:26 -0700

    Providence-student_task

---
+++ studentform.js
@@ -0,0 +1,9 @@
+function submitForm(){
+    const name = document.getElementById("form_input").value;
+    console.log(name);
+    const phoneNo = document.getElementById("form_input").;
+    console.log(phoneNo);
+    const email = document.getElementById("form_input");
+    // console.log(email)
+
+}
