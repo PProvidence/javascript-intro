b3b2cc2c699bf49d6c06bef2610fb24033c1c344
PProvidence <providentialprov1@gmail.com>
2023-04-28 04:13:24 -0700

    form objects

--- studentform.js
+++ studentform.js
@@ -1,9 +1,23 @@
 function submitForm(){
-    const name = document.getElementById("form_input").value;
-    console.log(name);
-    const phoneNo = document.getElementById("form_input").;
-    console.log(phoneNo);
-    const email = document.getElementById("form_input");
-    // console.log(email)
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phoneNo").value;
+    const email = document.getElementById("email").value;
 
+    console.log(name);
+    console.log(phone);
+    console.log(email);
+
+const students ={
+    name: name,
+    phone: phone,
+    email: email,
+
+    getStudentDetails: function ( ) {
+        return `The name of the student is ${this.name} and his phone no. is ${phone}`
+    }
 }
+console.log(students);
+console.log(students.getStudentDetails());
+}
+
+// read getters and setters
